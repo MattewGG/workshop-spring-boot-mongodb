629e4b66fa4da19eaf81cf9d57d8ae2a2aa1e405
mateusgasda@gmail.com <mateusgasda@gmail.com>
2024-11-08 13:58:52 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/mateusweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mateusweb/workshopmongo/resources/UserResource.java
@@ -1,6 +1,9 @@
 package com.mateusweb.workshopmongo.resources;
 
 import com.mateusweb.workshopmongo.domain.User;
+import com.mateusweb.workshopmongo.repository.UserRepository;
+import com.mateusweb.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +18,13 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     //@GetMapping
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "maria brown", "maria@gmail.com");
-        User alex = new User("2", "alex green", "alex@gmail.com");
-        List<User> list = new ArrayList<User>();
-        list.addAll(Arrays.asList(maria, alex));
+
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/mateusweb/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.mateusweb.workshopmongo.services;
+
+import com.mateusweb.workshopmongo.domain.User;
+import com.mateusweb.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
